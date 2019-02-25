362c6bf7fa08d2f87711fd1ffa6ef8264f8cb884
Sandra Padua <sandrapadua5@gmail.com>
2019-02-25 14:31:56 +0100

    Changed the text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to React.
+          Welcome to Codaisseur.
           </p>
           <a
             className="App-link"
